10b3e83355df0b031431f439721267540d526c54
Raghib Haque <raghib.haque.2007@gmail.com>
2026-04-20 09:26:51 +0100

    add git ignore and add book class

--- Main.java
+++ Main.java
@@ -1,7 +1,33 @@
 import java.util.Scanner;
 
 
+class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
     }
 }
