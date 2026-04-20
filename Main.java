481b26337535b4b968fc6b691a23ff6fc2490a43
Raghib Haque <raghib.haque.2007@gmail.com>
2026-04-20 09:28:00 +0100

    Add scanner to take in parameters for book object

--- Main.java
+++ Main.java
@@ -28,6 +28,17 @@ class Book {
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        System.out.print("Enter book title: ");
+        String title = sc.nextLine();
+        System.out.print("Enter book author: ");
+        String author = sc.nextLine();
+        System.out.print("Enter book price: ");
+        double price = sc.nextDouble();
+        Book book = new Book(title, author, price);
+        System.out.println("Book Details:");
+        System.out.println("Title: " + book.getTitle());
+        System.out.println("Author: " + book.getAuthor());
+        System.out.println("Price: $" + book.getPrice());
+
     }
 }
